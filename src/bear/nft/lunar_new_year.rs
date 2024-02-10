e0a45029bab2a45845e1b33a6303728de1bb4442
Davirain <davirain.yin@gmail.com>
2024-02-10 18:14:26 +0800

    update

--- src/bear/nft/lunar_new_year.rs
+++ src/bear/nft/lunar_new_year.rs
@@ -4,7 +4,7 @@ use ethers::prelude::*;
 use std::str::FromStr;
 use std::sync::Arc;
 
-fn lunar_new_year() -> H160 {
+pub fn lunar_new_year_addr() -> H160 {
     H160::from_str("0xDc094eaC7CC01224E798F34543a8F9e9D2559479").unwrap()
 }
 
@@ -17,8 +17,8 @@ abigen!(
 
 ///
 pub async fn total_supply(client: &Client) -> Result<U256, Box<dyn std::error::Error>> {
-    let contract = LunarNewYearModule::new(lunar_new_year(), Arc::new(client.clone()));
-    let value = contract.total_supply().await?;
+    let contract = LunarNewYearModule::new(lunar_new_year_addr(), Arc::new(client.clone()));
+    let value = contract.total_supply().call().await?;
     Ok(value)
 }
 
@@ -28,9 +28,11 @@ pub async fn approve(
     account: Address,
     amout: U256,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let contract = LunarNewYearModule::new(lunar_new_year(), Arc::new(client.clone()));
-    let value = contract.approve(account, amout).await?;
-    Ok(value)
+    let contract = LunarNewYearModule::new(lunar_new_year_addr(), Arc::new(client.clone()));
+    let tx = contract.approve(account, amout).send().await?.await?;
+    println!("Transaction Receipt: {}", serde_json::to_string(&tx)?);
+
+    Ok(())
 }
 
 ///
@@ -38,7 +40,16 @@ pub async fn balance_of(
     client: &Client,
     owner: Address,
 ) -> Result<U256, Box<dyn std::error::Error>> {
-    let contract = LunarNewYearModule::new(lunar_new_year(), Arc::new(client.clone()));
-    let value = contract.balance_of(owner).await?;
+    let contract = LunarNewYearModule::new(lunar_new_year_addr(), Arc::new(client.clone()));
+    let value = contract.balance_of(owner).call().await?;
     Ok(value)
+}
+
+/// buy
+pub async fn buy(client: &Client) -> Result<(), Box<dyn std::error::Error>> {
+    let contract = LunarNewYearModule::new(lunar_new_year_addr(), Arc::new(client.clone()));
+    let tx = contract.buy().send().await?.await?;
+    println!("Transaction Receipt: {}", serde_json::to_string(&tx)?);
+
+    Ok(())
 }
--- src/command/bear/nft.rs
+++ src/command/bear/nft.rs
@@ -1,4 +1,5 @@
-use crate::bear::nft::lunar_new_year;
+use crate::bear::deploy_contracts::honey::{self, honey_token_addr};
+use crate::bear::nft::lunar_new_year::{self, lunar_new_year_addr};
 use crate::errors::Error;
 use crate::utils::{get_all_keypairs, get_config, get_single_keypairs};
 use ethers::prelude::SignerMiddleware;
@@ -21,7 +22,8 @@ impl NFT {
         let provider = Provider::<Http>::try_from(config.rpc_endpoint)
             .map_err(|e| Error::Custom(e.to_string()))?;
 
-        let keypairs = get_single_keypairs().map_err(|e| Error::Custom(e.to_string()))?;
+        let keypairs =
+            get_all_keypairs(&self.file_name).map_err(|e| Error::Custom(e.to_string()))?;
 
         for keypair in &keypairs.keypairs {
             let client = SignerMiddleware::new(
@@ -38,7 +40,53 @@ impl NFT {
                 .await
                 .map_err(|e| Error::Custom(e.to_string()))?;
 
-            println!("Address({:?}) have {} num", keypair.address(), balance);
+            println!(
+                "Address({:?}) have {} Lunar New Year NFT num",
+                keypair.address(),
+                balance
+            );
+
+            let honey_balance = honey::balance_of(&client, keypair.address())
+                .await
+                .map_err(|e| Error::Custom(e.to_string()))?;
+
+            println!(
+                "Address({:?}) have {} honey num",
+                keypair.address(),
+                balance
+            );
+
+            let approve_result = honey::approve(&client, lunar_new_year_addr(), honey_balance)
+                .await
+                .map_err(|e| Error::Custom(e.to_string()))?;
+
+            println!("approve_result: {:?}", approve_result);
+
+            let buy_result = lunar_new_year::buy(&client)
+                .await
+                .map_err(|e| Error::Custom(e.to_string()))?;
+
+            println!("buy_result: {:?}", approve_result);
+
+            let balance = lunar_new_year::balance_of(&client, keypair.address())
+                .await
+                .map_err(|e| Error::Custom(e.to_string()))?;
+
+            println!(
+                "Address({:?}) have {}  num Lunar New Year NFT",
+                keypair.address(),
+                balance
+            );
+
+            let honey_balance = honey::balance_of(&client, keypair.address())
+                .await
+                .map_err(|e| Error::Custom(e.to_string()))?;
+
+            println!(
+                "Address({:?}) have {} honey num",
+                keypair.address(),
+                balance
+            );
         }
 
         Ok(())
