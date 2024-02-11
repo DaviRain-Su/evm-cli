fc18e5455db56c6ebdac0c11d65794051b6b3508
Davirain <davirain.yin@gmail.com>
2024-02-11 22:58:57 +0800

    Update liquidity.rs

--- src/command/bear/dex/liquidity.rs
+++ src/command/bear/dex/liquidity.rs
@@ -8,6 +8,7 @@ use crate::bear::precompile_contracts::{
     erc20_bank::{self, erc20_bank_addr},
     erc20_dex::{self, erc20_dex_addr},
 };
+use crate::constant::BERA_DECIMAL;
 use crate::errors::Error;
 use crate::utils::{get_all_keypairs, get_config, get_single_keypairs};
 use ethers::prelude::SignerMiddleware;
@@ -47,13 +48,18 @@ impl Liquidity {
                 keypair.clone().with_chain_id(self.chain_id),
             );
 
-            // let block_number = BlockId::from(BlockNumber::Finalized);
             let balance = provider
                 .get_balance(keypair.address(), None)
                 .await
                 .map_err(|e| Error::Custom(e.to_string()))?;
 
-            println!("Address({:?}) have {}", keypair.address(), balance);
+            let native_balance_f64 = balance.as_u128() as f64 / BERA_DECIMAL;
+
+            println!(
+                "Address({}) have {} Bera",
+                keypair.address(),
+                native_balance_f64
+            );
 
             let pool_id: Address = "0xa88572F08f79D28b8f864350f122c1CC0AbB0d96"
                 .parse()
@@ -63,12 +69,6 @@ impl Liquidity {
                 .map_err(|e| Error::Custom(e.to_string()))?;
 
             println!("this address {:?} pool name is {}", pool_id, pool_name);
-
-            let liquidity = erc20_dex::get_liquidity(&client, pool_id.clone())
-                .await
-                .map_err(|e| Error::Custom(e.to_string()))?;
-
-            println!("this address {:?} liquidity {:?}", pool_id, liquidity);
 
             let kind = 0;
             let base_asset: Address = "0x5806e416da447b267cea759358cf22cc41fae80f"
@@ -81,6 +81,8 @@ impl Liquidity {
                 "Base Asset({:?}) balance: {}",
                 base_asset, base_asset_amount
             );
+
+            let half_base_asset_amount = base_asset_amount / 2;
 
             let wbera_addr_result = wbera::approve(&client, wbera_addr(), base_asset_amount)
                 .await
@@ -95,49 +97,21 @@ impl Liquidity {
                 kind,
                 pool_id,
                 base_asset,
-                base_asset_amount / 100,
+                half_base_asset_amount,
                 quote_asset,
             )
             .await
             .map_err(|e| Error::Custom(e.to_string()))?;
             println!("preview swap: {:?}", preview_swap);
 
-            let deadline = U256::from(get_epoch_milliseconds()) + U256::from(60 * 1000);
-
-            // todo(davirain)
-            // ERROR(Error: Custom("Contract call reverted with data: 0x"))
-
-            let result_swap = erc20_dex::swap(
-                &client,
-                kind,
-                pool_id,
-                base_asset,
-                base_asset_amount / 100,
-                preview_swap.0,
-                preview_swap.1,
-                deadline,
-            )
-            .await
-            .map_err(|e| Error::Custom(e.to_string()))?;
-            println!("swap: {:?}", result_swap);
-
-            // let total_shares = erc20_dex::get_total_shares(&client, pool_id.clone())
-            //     .await
-            //     .map_err(|e| Error::Custom(e.to_string()))?;
-            // println!("total shares: {:?}", total_shares);
-
-            // let exchange_rate =
-            //     erc20_dex::get_exchange_rate(&client, pool_id.clone(), base_asset, quote_asset)
-            //         .await
-            //         .map_err(|e| Error::Custom(e.to_string()))?;
-
-            // println!("the exchange rate is {:?}", exchange_rate);
-
-            // let options = erc20_dex::get_pool_options(&client, pool_id.clone())
-            //     .await
-            //     .map_err(|e| Error::Custom(e.to_string()))?;
-
-            // println!("pool options: {:?}", options);
+            let receiver = keypair.address();
+            let asset_in = vec![base_asset, quote_asset];
+            let asset_amount = vec![half_base_asset_amount, preview_swap.1];
+            let result_add_liquidity =
+                erc20_dex::add_liquidity(&client, pool_id, receiver, asset_in, asset_amount)
+                    .await
+                    .map_err(|e| Error::Custom(e.to_string()))?;
+            println!("add_liquidity: {:?}", result_add_liquidity);
         }
         Ok(())
     }
