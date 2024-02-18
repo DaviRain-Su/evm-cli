34ad3daff3d3bc5abd421c312e0295acb5ab402b
Davirain <davirain.yin@gmail.com>
2024-02-18 13:37:21 +0800

    Update keys.rs

--- src/command/keys.rs
+++ src/command/keys.rs
@@ -58,13 +58,13 @@ impl NewFormat {
         let all_keypairs =
             get_all_keypairs(&self.file_name).map_err(|e| Error::Custom(e.to_string()))?;
 
-        let keyparis_str = KeyPairsString::convert_to_new_format(all_keypairs);
+        let keyparis_str = KeyPairsStringWithbalance::from(all_keypairs);
 
         let home_path = dirs::home_dir().ok_or(Error::Custom("can't open home dir".into()))?;
         let keypairs_path = home_path
             .join(".config")
             .join("evm-cli")
-            .join(format!("{}_convert.json", self.file_name));
+            .join(format!("{}_new_format.json", self.file_name));
 
         keyparis_str
             .write(keypairs_path.clone())
@@ -216,8 +216,6 @@ pub struct KeyPairsString {
 pub struct Item {
     pub pubkey: String,
     pub secret: String,
-    pub balance: String,
-    pub verify: bool,
 }
 
 impl KeyPairsString {
@@ -260,8 +258,6 @@ impl From<KeyPairs> for KeyPairsString {
                     pubkey: format!("{:?}", k.address()),
                     secret: serde_json::to_string(&raw_keypairs.to_bytes().to_vec())
                         .expect("failed ser"),
-                    balance: String::new(),
-                    verify: false,
                 }
             })
             .collect::<Vec<_>>();
@@ -286,8 +282,6 @@ impl KeyPairsString {
                 Item {
                     pubkey: format!("{:?}", k.address()),
                     secret: secret_hex,
-                    balance: String::new(),
-                    verify: false,
                 }
             })
             .collect::<Vec<_>>();
@@ -304,8 +298,6 @@ impl KeyPairsString {
                     pubkey: format!("{:?}", k.address()),
                     secret: serde_json::to_string(&raw_keypairs.to_bytes().to_vec())
                         .expect("failed ser"),
-                    balance: String::new(),
-                    verify: false,
                 }
             })
             .collect::<Vec<_>>();
@@ -339,5 +331,67 @@ impl KeyPairs {
 
     pub fn push(&mut self, keypair: LocalWallet) {
         self.keypairs.push(keypair);
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyPairsStringWithbalance {
+    pub keypairs: Vec<ItemWithBalance>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ItemWithBalance {
+    pub pubkey: String,
+    pub secret: String,
+    pub balance: String,
+    pub verify: bool,
+}
+
+impl KeyPairsStringWithbalance {
+    pub fn write(&self, path: PathBuf) -> anyhow::Result<()> {
+        let temp_keypairs_str = serde_json::to_string(&self)?;
+        std::fs::write(path, temp_keypairs_str).map_err(|e| {
+            Error::from(format!(
+                "failed write keypairs_path: Error({})",
+                e.to_string()
+            ))
+        })?;
+        Ok(())
+    }
+
+    pub fn read(path: PathBuf) -> anyhow::Result<Self> {
+        let temp_keypairs_str = std::fs::read_to_string(path).map_err(|e| {
+            Error::from(format!(
+                "failed read keypairs_path: Error({})",
+                e.to_string()
+            ))
+        })?;
+        let keypairs_str = serde_json::from_str(&temp_keypairs_str).map_err(|e| {
+            Error::from(format!(
+                "failed deserialze keypairs_path: Error({})",
+                e.to_string()
+            ))
+        })?;
+        Ok(keypairs_str)
+    }
+}
+
+impl From<KeyPairs> for KeyPairsStringWithbalance {
+    fn from(keypairs: KeyPairs) -> Self {
+        let keypairs = keypairs
+            .keypairs
+            .iter()
+            .map(|k| {
+                let raw_keypairs = k.signer();
+                ItemWithBalance {
+                    pubkey: format!("{:?}", k.address()),
+                    secret: serde_json::to_string(&raw_keypairs.to_bytes().to_vec())
+                        .expect("failed ser"),
+                    balance: String::new(),
+                    verify: false,
+                }
+            })
+            .collect::<Vec<_>>();
+        Self { keypairs }
     }
 }
